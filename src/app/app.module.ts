0bde1351e1f4512f4e356ed246a4f3dcc212f7a3
Abhijit Kar <ak24284@gmail.com>
2018-01-14 01:24:15 +0530

    Multiple Features Enabled
    1. Manifest Added
    2. Service Worker enabled
    3. Angular Universal Integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { ServiceWorkerModule } from "@angular/service-worker";
+
+import { environment } from "../environments/environment";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule.withServerTransition({ appId: "dont-let-him-pou" }),
+    ServiceWorkerModule.register("/ngsw-worker.js", {
+      enabled: environment.production
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
