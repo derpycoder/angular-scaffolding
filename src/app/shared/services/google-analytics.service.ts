fcce863dfaa68ef300d18f78f51d0e261ad32ce2
Abhijit Kar <ak24284@gmail.com>
2018-01-14 12:08:55 +0530

    Minute correction

--- src/app/shared/services/google-analytics.service.ts
+++ src/app/shared/services/google-analytics.service.ts
@@ -5,7 +5,7 @@ declare var ga;
 
 @Injectable()
 export class GoogleAnalyticsService {
-  googleAnalyticsKey: string = "UA-112417282-1";
+  private googleAnalyticsKey: string = "UA-112417282-1";
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
     this.initGoogleAnalytics();
