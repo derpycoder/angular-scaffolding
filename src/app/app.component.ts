662d7e8818942f6f2d4880e08a2e2b855568d4dc
Abhijit Kar <ak24284@gmail.com>
2018-05-16 00:18:14 +0530

    SEO Part 3: Meta tag inclusion done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { Title } from "@angular/platform-browser";
+import { Title, Meta } from "@angular/platform-browser";
 import { Router, NavigationEnd } from "@angular/router";
 
 import { GoogleAnalyticsService } from "./shared/";
@@ -11,23 +11,53 @@ import { GoogleAnalyticsService } from "./shared/";
 })
 export class AppComponent {
   routes = {
-    "/": "Home Page",
-    "/home": "Home Page",
-    "/about": "About Us",
-    "/contact": "Contact Us",
+    "/": {
+      title: "Home Page",
+      desc: "This is the description of Home Page"
+    },
+    "/home": {
+      title: "Home Page",
+      desc: "This is the description of Home Page"
+    },
+    "/about": {
+      title: "About Us",
+      desc: "This page will describe some things about us"
+    },
+    "/contact": {
+      title: "Contact Us",
+      desc: "This page will tell you how to contact us"
+    }
   };
 
   constructor(
     public router: Router,
     private googleAnalyticsService: GoogleAnalyticsService,
-    private titleService: Title
+    private titleService: Title,
+    private meta: Meta
   ) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        this.titleService.setTitle(this.routes[event.url]);
+        this.handleSEO(event);
 
         this.googleAnalyticsService.emitPageEvent(event);
       }
     });
+  }
+
+  handleSEO(event: NavigationEnd) {
+    const desc = this.routes[event.url].desc;
+
+    // Generic
+    this.titleService.setTitle(this.routes[event.url].title);
+    this.meta.addTag({ name: "description", content: desc });
+
+    // Twitter Metadata
+    this.meta.addTag({ name: "twitter:card", content: "summary_large_image" });
+    this.meta.addTag({ name: "twitter:site", content: "@AbhijitKarDikha" });
+    this.meta.addTag({ name: "twitter:creator", content: "@AbhijitKarDikha" });
+    this.meta.addTag({ name: "twitter:title", content: desc });
+    this.meta.addTag({ name: "twitter:description", content: desc });
+    this.meta.addTag({ name: "twitter:text:description", content: desc });
+    this.meta.addTag({ name: "twitter:image", content: "https://www.abhijit-kar.com/assets/meta/screenshot.png" });
   }
 }
