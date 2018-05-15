616f002a9e23981824e6e1039beae3a6aef54a55
Abhijit Kar <ak24284@gmail.com>
2018-05-16 00:00:03 +0530

    SEO Step 2: Title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from "@angular/core";
+import { Title } from "@angular/platform-browser";
 import { Router, NavigationEnd } from "@angular/router";
-import { GoogleAnalyticsService } from "./shared/";
 
-declare var ga;
+import { GoogleAnalyticsService } from "./shared/";
 
 @Component({
   selector: "app-root",
@@ -10,12 +10,22 @@ declare var ga;
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  routes = {
+    "/": "Home Page",
+    "/home": "Home Page",
+    "/about": "About Us",
+    "/contact": "Contact Us",
+  };
+
   constructor(
     public router: Router,
-    private googleAnalyticsService: GoogleAnalyticsService
+    private googleAnalyticsService: GoogleAnalyticsService,
+    private titleService: Title
   ) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
+        this.titleService.setTitle(this.routes[event.url]);
+
         this.googleAnalyticsService.emitPageEvent(event);
       }
     });
